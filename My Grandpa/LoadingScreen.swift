9e7b49b2c9fdbdd8dbf4bf17a10291607cc19171
RickctvDev <rickcrane@Ricks-MacBook-Pro.local>
2017-04-26 00:03:25 +0200

    first commit

---
+++ My Grandpa/LoadingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  My Grandpa
+//
+//  Created by Rick Crane on 25/04/2017.
+//  Copyright © 2017 Rick Crane. All rights reserved.
+//
+
+import GameplayKit
+import AVFoundation
+
+class LoadingScreen: SKScene {
+    
+    override func didMove(to view: SKView) {
+        print("On Loading Scene ACTUAL")
+    }
+
+}
---
+++ My Grandpa/MainMenu.swift
@@ -0,0 +1,9 @@
+//
+//  MainMenu.swift
+//  My Grandpa
+//
+//  Created by Rick Crane on 25/04/2017.
+//  Copyright © 2017 Rick Crane. All rights reserved.
+//
+
+import Foundation
